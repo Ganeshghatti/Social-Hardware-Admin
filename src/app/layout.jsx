aa2a77ce8eb3a097950aa8b1f089f2b633cb7634
GaneshGhatti <ganeshghatti6@gmail.com>
2024-11-19 19:27:23 +0530

    favicon

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -4,6 +4,31 @@ import './globals.css';
 export const metadata = {
   title: 'Blog Admin',
   description: 'Blog Admin Dashboard',
+  icons: {
+    icon: [
+      {
+        url: '/assets/images/favicon.ico',
+        sizes: '32x32',
+      },
+      {
+        url: '/assets/images/favicon-16x16.png',
+        sizes: '16x16',
+        type: 'image/png',
+      },
+      {
+        url: '/assets/images/favicon-32x32.png',
+        sizes: '32x32',
+        type: 'image/png',
+      },
+    ],
+    apple: [
+      {
+        url: '/assets/images/apple-touch-icon.png',
+        sizes: '180x180',
+        type: 'image/png',
+      },
+    ],
+  },
 };
 
 export default function RootLayout({ children }) {
