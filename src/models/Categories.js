f4d00fbc346d2d6d77f86fad0608aa0a1c905a46
GaneshGhatti <ganeshghatti6@gmail.com>
2024-12-02 13:21:40 +0530

    category model issue

---
+++ src/models/Categories.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+import Blog from "./Blog"; // Import Blog model
+
+const CategorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please provide a Category"],
+      unique: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Add pre-remove middleware to clean up blog references
+CategorySchema.pre(
+  "deleteOne",
+  { document: false, query: true },
+  async function () {
+    const categoryId = this.getFilter()._id;
+    await Blog.updateMany(
+      { category: categoryId },
+      { $pull: { category: categoryId } }
+    );
+  }
+)
+
+export default mongoose.models.Category || mongoose.model('Category', CategorySchema);
