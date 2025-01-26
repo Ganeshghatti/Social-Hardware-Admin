8590ca2fa24e5cf22abcb12b7d7814e0c876aebf
GaneshGhatti <ganeshghatti6@gmail.com>
2025-01-26 12:47:32 +0530

    add sitemap.xml and robots.txt

--- src/app/api/leads/email/callback/route.js
+++ src/app/api/leads/email/callback/route.js
@@ -16,33 +16,31 @@ export async function OPTIONS() {
 }
 
 export async function POST(request) {
+  // Apply CORS headers to the response
+  const response = await handlePost(request);
+  return cors(response);
+}
+
+async function handlePost(request) {
   try {
-    // Connect to MongoDB
     await dbConnect();
-
-    // Parse the incoming JSON data
     const data = await request.json();
-
-    // Log the received data
     console.log("Webhook data received:", data);
 
     for (const item of data.batch) {
       await Leads.updateOne(
-        { _id: item._id }, // Match by ID
-        { $set: { email: item.emails } } // Update the `emails` field
+        { _id: item._id },
+        { $set: { email: item.emails } }
       );
     }
 
-    console.log("updated email")
-
+    console.log("updated email");
     return NextResponse.json(
       { message: "Email updated successfully" },
       { status: 200 }
     );
   } catch (error) {
     console.error("Error processing webhook:", error);
-
-    // Return error response
     return NextResponse.json(
       { message: "Error processing webhook", error: error.message },
       { status: 500 }
